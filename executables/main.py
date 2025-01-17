26a8971705c9052f9a90bd508d28355268d1e649
Stingy37 <166416791+Stingy37@users.noreply.github.com>
2025-01-17 00:42:53 +0000

    edited main.py to differentiate it from new code development

--- executables/main.py
+++ executables/main.py
@@ -6,11 +6,11 @@ import asyncio
 from modules.utils import initialize_environment
 from modules.livestream_manager import generate_livestream
 
-def main_program():
+def main_generate_livestream():
   # sets up environment for running code
   initialize_environment()
 
   # this provides all the media necessary for livestream - a TTS voice, images, key messages, etc.
   asyncio.run(generate_livestream(audio_already_playing = False))
 
-main_program()
+main_generate_livestream()
